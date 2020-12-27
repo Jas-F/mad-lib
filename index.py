ea6a111a4ac126046a6bac0f8e6e4a9889dd0c80
Jas-F <jasminer.franklin@gmail.com>
2020-12-27 03:44:22 -0500

    add f string

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 exclamation = 
-adverb =
+verb =
 noun =
+noun2 = 
 adjective =
-exclamation = 
+
+madLib = f"Hi! i am {verb} how to code in order to demonstrate that i have skills in Python.\
+{noun} is the {adjective} coding language and used in {noun2}."
